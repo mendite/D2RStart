a5ecd7813fa5f918512ff7621c287428cdb3c55f
BST <mendite@mendite.de>
2023-01-10 22:55:07 +0100

    Added hint window for download Handle at Microsoft

--- D2RStart/App.xaml.cs
+++ D2RStart/App.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Reflection;
 
 namespace D2RStart
 {
@@ -13,5 +15,21 @@ namespace D2RStart
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            string handle64ExeFile = Path.Combine(AppContext.BaseDirectory, "handle64.exe");
+
+            //Assembly.GetExecutingAssembly().
+            if (File.Exists(handle64ExeFile))
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+            }
+            else
+            {
+                DownloadHandle64ExeWindow downloadHandle64ExeWindow = new DownloadHandle64ExeWindow();
+                downloadHandle64ExeWindow.Show();
+            }
+        }
     }
 }
---
+++ D2RStart/DownloadHandle64ExeWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using D2RStart.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace D2RStart
+{
+    /// <summary>
+    /// Interaction logic for DownloadHandle64ExeWindow.xaml
+    /// </summary>
+    public partial class DownloadHandle64ExeWindow : Window
+    {
+        public DownloadHandle64ExeWindow()
+        {
+            InitializeComponent();
+            SourceInitialized += DownloadHandle64ExeWindow_SourceInitialized;
+        }
+
+        private void DownloadHandle64ExeWindow_SourceInitialized(object sender, EventArgs e)
+        {
+            this.HideMinimizeAndMaximizeButtons();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            try
+            {
+                //Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));                
+                //Process.Start($"{e.Uri.AbsoluteUri}");
+                System.Diagnostics.Process.Start("cmd", $"/c start {e.Uri.AbsoluteUri}");
+                e.Handled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex?.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
